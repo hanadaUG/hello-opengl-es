9a53eae1b15bb2065efb65e68598da3229083a0c
ayaseya <ayaseya@gmail.com>
2020-02-14 00:49:56 +0900

    ログとコメントを追加

--- android/app/src/main/java/com/es/opengl/hello/MainActivity.java
+++ android/app/src/main/java/com/es/opengl/hello/MainActivity.java
@@ -4,16 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
+import android.util.Log;
 
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
 public class MainActivity extends AppCompatActivity {
+    static final String TAG = "XXXXX";
 
+    // EGL関連の初期化を代行してくれる
     private GLSurfaceView glView = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate() E");
         super.onCreate(savedInstanceState);
 
         // OpenGL用に拡張されたSurfaceViewを用意する
@@ -24,51 +28,62 @@ public class MainActivity extends AppCompatActivity {
 
         // GLSurfaceViewをActivityに設定し表示する
         setContentView(glView);
+        Log.d(TAG, "onCreate() X");
     }
 
     @Override
     protected void onResume() {
+        Log.d(TAG, "onResume() E");
         super.onResume();
         glView.onResume();
+        Log.d(TAG, "onResume() X");
     }
 
     @Override
     protected void onPause() {
+        Log.d(TAG, "onPause() E");
         super.onPause();
         glView.onPause();
+        Log.d(TAG, "onPause() X");
     }
 
     @Override
     protected void onDestroy() {
+        Log.d(TAG, "onDestroy() E");
         super.onDestroy();
+        Log.d(TAG, "onDestroy() X");
     }
 
     // 描画を担当するクラス
     private static GLSurfaceView.Renderer GLRender = new GLSurfaceView.Renderer() {
+        // 初期化時
         @Override
         public void onSurfaceCreated(GL10 gl10, EGLConfig eglConfig) {
-
+            Log.d(TAG, "onSurfaceCreated() E");
+            Log.d(TAG, "onSurfaceCreated() X");
         }
 
+        // 画面のサイズが変更された時（端末の回転時などに呼び出される）
         @Override
         public void onSurfaceChanged(GL10 gl10, int w, int h) {
+            Log.d(TAG, "onSurfaceChanged() E");
             // 画面の左下の原点0,0から画面の幅・高さのピクセル範囲にかけて描画を許可する
             gl10.glViewport(0, 0, w, h);
+            Log.d(TAG, "onSurfaceChanged() X");
         }
 
+        // 毎フレーム呼び出される
         @Override
         public void onDrawFrame(GL10 gl10) {
+//            Log.d(TAG, "onDrawFrame() E");
             // 画面を塗り潰す色情報を指定する
             // RGBA(255, 0, 0, 255)
             gl10.glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
 
-            // OpenGLでは、すべてのピクセルの情報を保存する領域をバッファと呼び
-            // ピクセルに関する1ビットの情報を保存するバッファをビットプレーンと呼びます
-            // glClearはglClear◯◯というメソッドにより指定された情報を
-            // 指定したバッファ領域のビットプレーンに反映します
+            // OpenGLでは、すべてのピクセル情報を保存する領域をバッファと呼びます
+            // カラーバッファを塗り潰す（クリアする）
             gl10.glClear(GL10.GL_COLOR_BUFFER_BIT);
+//            Log.d(TAG, "onDrawFrame() X");
         }
     };
-
-
 }
